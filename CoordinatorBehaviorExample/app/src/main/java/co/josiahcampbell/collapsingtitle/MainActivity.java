370691817b486d184d70fe816d864c2218521254
Josiah Campbell <josiahcampbell64@gmail.com>
2016-11-22 11:56:11 -0500

    Update actions

--- CoordinatorBehaviorExample/app/src/main/java/co/josiahcampbell/collapsingtitle/MainActivity.java
+++ CoordinatorBehaviorExample/app/src/main/java/co/josiahcampbell/collapsingtitle/MainActivity.java
@@ -1,5 +1,8 @@
 package co.josiahcampbell.collapsingtitle;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v4.content.ContextCompat;
@@ -7,22 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import static android.view.View.VISIBLE;
 
 public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
-    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.65f;
-    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.7f;
-    private static final int ALPHA_ANIMATIONS_DURATION = 200;
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.25f;
+    private static final int ALPHA_ANIMATIONS_DURATION = 100;
 
     private boolean isTitleVisible;
-    private boolean isTitleContainerVisible;
 
-    private LinearLayout titleContainer;
     private TextView title;
     private AppBarLayout appBarLayout;
     private Toolbar toolbar;
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
         setContentView(R.layout.activity_main);
 
         isTitleVisible = false;
-        isTitleContainerVisible = true;
 
         bindActivity();
 
@@ -46,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
     private void bindActivity() {
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         title = (TextView) findViewById(R.id.main_textview_title);
-        titleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
         appBarLayout = (AppBarLayout) findViewById(R.id.main_appbar);
     }
 
@@ -62,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
         int maxScroll = appBarLayout.getTotalScrollRange();
         float percentage = (float) Math.abs(offset) / (float) maxScroll;
 
-        handleAlphaOnTitle(percentage);
+//        handleAlphaOnTitle(percentage);
         handleToolbarTitleVisibility(percentage);
     }
 
@@ -71,25 +69,11 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
             if (!isTitleVisible) {
                 startAlphaAnimation(title, ALPHA_ANIMATIONS_DURATION, VISIBLE);
                 isTitleVisible = true;
+                toolbar.setBackground(ContextCompat.getDrawable(this, R.color.primary));
             }
         } else if (isTitleVisible) {
             startAlphaAnimation(title, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
             isTitleVisible = false;
-        }
-    }
-
-    private void handleAlphaOnTitle(float percentage) {
-        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if (isTitleContainerVisible) {
-                startAlphaAnimation(titleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                isTitleContainerVisible = false;
-            }
-            toolbar.setBackground(ContextCompat.getDrawable(this, R.color.primary));
-        } else {
-            if (!isTitleContainerVisible) {
-                startAlphaAnimation(titleContainer, ALPHA_ANIMATIONS_DURATION, VISIBLE);
-                isTitleContainerVisible = true;
-            }
             toolbar.setBackground(null);
         }
     }
@@ -101,7 +85,6 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
         } else {
             alphaAnimation = new AlphaAnimation(1f, 0f);
         }
-
         alphaAnimation.setDuration(duration);
         alphaAnimation.setFillAfter(true);
         view.startAnimation(alphaAnimation);
