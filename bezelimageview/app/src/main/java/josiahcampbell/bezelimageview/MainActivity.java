07bb5cf69cb88010a51cc250a59a7b7ebfef7b1a
Josiah Campbell <josiahcampbell64@gmail.com>
2016-12-01 12:48:54 -0500

    Add BezelImageView test

---
+++ bezelimageview/app/src/main/java/josiahcampbell/bezelimageview/MainActivity.java
@@ -0,0 +1,19 @@
+package josiahcampbell.bezelimageview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+
+        Picasso.with(this).load(R.drawable.lorempixel).into(imageView);
+    }
+}
